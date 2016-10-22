729871fa050a2ccce0bf7c092f8e5cb1ca66cf25
Daniel Miles <daniel.t.miles@gmail.com>
2016-10-22 11:08:01 -0700

    adding basic in-memory data store class-stub with tests

---
+++ dao/in_memory_data_store.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class InMemoryDataStore:
+    def __init__(self):
+        self._store = {}
+
+    def create(self, id, data):
+        pass
+
+    def retrieve(self, id):
+        pass
+
+    def update(self, id, data):
+        pass
+
+    def delete(self, id):
+        pass
---
+++ dao/test_in_memory_data_store.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import unittest
+from in_memory_data_store import InMemoryDataStore
+
+class TestInMemoryDataStore(unittest.TestCase):
+
+    def setUp(self):
+        self.store = InMemoryDataStore()
+
+    def test_create(self):
+        self.store.create(1, {"key": "value"})
+        self.assertEqual(self.store._store[1], {"key": "value"})
+
+    def test_retrieve(self):
+        self.store.create(1, {"key": "value"})
+        retrieved = self.store.retrieve(1)
+        self.assertEqual(retrieved, {"key": "value"})
+
+    def test_update(self):
+        self.store.create(1, {"key": "value"})
+        self.store.update(1, {"second_key": "second_value"})
+        self.assertEqual(self.store._store, {"key": "value", "second_key": "second_value"})
+
+    def test_delete(self):
+        self.store.create(1, {"key": "value"})
+        self.assertEqual(self.store._store[1], {"key": "value"})
+        self.store.delete(1)
+        self.assertEqual(self.store._store, {})
+
+
+if __name__ == '__main__':
+    unittest.main()
