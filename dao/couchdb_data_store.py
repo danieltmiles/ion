baf5b5ae359f98587849e13e7e0004feec9fe6da
Daniel Miles <daniel.t.miles@gmail.com>
2016-10-22 17:43:56 -0700

    adding pybreaker circuit breakers to couchdb data store

--- dao/couchdb_data_store.py
+++ dao/couchdb_data_store.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 import json
+import pybreaker
 import requests
+from in_memory_data_store import InMemoryDataStore
+
+class ReplayListener(pybreaker.CircuitBreakerListener):
+    def __init__(self, couchDataStore):
+        self.couchDataStore = couchDataStore
+
+    def state_change(self, cb, old_state, new_state):
+        if new_state.name == "open":
+            for k in self.couchDataStore.cache.keys():
+                self.couchDataStore.create(k, self.couchDataStore.cache.retrieve(k))
+            self.couchDataStore.cache = InMemoryDataStore()
+
+breaker = pybreaker.CircuitBreaker(fail_max=3, reset_timeout=3)
+
 
 class CouchdbDataStore:
 
     def __init__(self, server_base_url, db_name, create=True):
         self.server_base_url = server_base_url
         self.db_name = db_name
+        self.cache = InMemoryDataStore()
         if create:
             resp = requests.get(self.server_base_url + "/_all_dbs")
             if resp.status_code != 200:
@@ -15,14 +31,32 @@ class CouchdbDataStore:
             if self.db_name not in databases:
                 uri = "%s/%s" % (self.server_base_url, self.db_name)
                 requests.put(uri)
+        breaker.add_listener(ReplayListener(self))
 
     def create(self, obj_id, data):
+        try:
+            return self._create(obj_id, data)
+        except pybreaker.CircuitBreakerError:
+            return self._create_fallback(obj_id, data)
+
+    @breaker
+    def _create(self, obj_id, data):
         uri = "%s/%s/%s" % (self.server_base_url, self.db_name, obj_id)
         resp = requests.put(uri, data=json.dumps(data))
         if resp.status_code != 200:
             raise Exception("unable to write to database (%s): %s" % (resp.status_code, resp.text))
 
+    def _create_fallback(self, obj_id, data):
+        self.cache.create(obj_id, data)
+
+    @breaker
     def retrieve(self, obj_id):
+        try:
+            return self._retrieve(obj_id)
+        except pybreaker.CircuitBreakerError:
+            return self._retrieve_fallback(obj_id)
+
+    def _retrieve(self, obj_id):
         uri = "%s/%s/%s" % (self.server_base_url, self.db_name, obj_id)
         resp = requests.get(uri)
         if resp.status_code != 200:
@@ -31,6 +65,9 @@ class CouchdbDataStore:
         del couch_data["_id"]
         del couch_data["_rev"]
         return couch_data
+
+    def _retrieve_fallback(self, obj_id):
+        return self.cache.retrieve(obj_id)
 
 
     #def update(self, obj_id, data):
